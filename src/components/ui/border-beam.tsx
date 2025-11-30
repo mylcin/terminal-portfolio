aa58827e13a4ca2282400c9130aeca97a9b64959
Mustafa Yalçın <74621802+mylcin@users.noreply.github.com>
2025-11-30 22:36:10 +0300

    fix: border-beam hidden on mobile

--- src/components/ui/border-beam.tsx
+++ src/components/ui/border-beam.tsx
@@ -66,7 +66,7 @@ export const BorderBeam = ({
 }: BorderBeamProps) => {
   return (
     <div
-      className="pointer-events-none absolute inset-0 rounded-[inherit] border-(length:--border-beam-width) border-transparent mask-[linear-gradient(transparent,transparent),linear-gradient(#000,#000)] mask-intersect [mask-clip:padding-box,border-box]"
+      className="pointer-events-none absolute inset-0 hidden rounded-[inherit] border-(length:--border-beam-width) border-transparent mask-[linear-gradient(transparent,transparent),linear-gradient(#000,#000)] mask-intersect [mask-clip:padding-box,border-box] md:block"
       style={
         {
           '--border-beam-width': `${borderWidth}px`,
